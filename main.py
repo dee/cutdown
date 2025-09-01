cd80379da0847016fe1a02f3b9472099c7e1060a
Dee <33315+dee@users.noreply.github.com>
2025-09-01 13:39:54 +0200

    Start and stop buttons which can change their state

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from ui.ui_mainwindow import Ui_MainWindow
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QDialog
 import pyperclip
 
 
 class MainWindow(QDialog):
+
+    can_start = pyqtSignal(bool)
+    can_stop = pyqtSignal(bool)
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.edtOutput.setText("output.mp4")
-        self.clipboard_content = ""
         self.ui.pbMain.setValue(0)
+        self.ui.btnStart.setDefaultAction(self.ui.actionStart)
+        self.ui.btnStop.setDefaultAction(self.ui.actionStop)
 
         # establish timer
         self.timer = QTimer(self)
@@ -19,12 +24,41 @@ class MainWindow(QDialog):
         self.timer.setInterval(500)
         self.timer.start()
 
+        # set up actions
+        self.ui.actionStart.triggered.connect(self.on_actionStart_triggered)
+        self.ui.actionStop.triggered.connect(self.on_actionStop_triggered)
+
+        # other slots
+        self.ui.edtClipboard.textChanged.connect(self.on_txtClipboard_changed)
+        self.can_start.connect(self.on_can_start)
+        self.can_stop.connect(self.on_can_stop)
+
+        # emit status signal
+        self.can_start.emit(True)
+        self.can_stop.emit(False)
+
     def on_timer(self):
         new_text = pyperclip.paste()
-        if new_text == self.clipboard_content:
+        if new_text == self.ui.edtClipboard.toPlainText():
             return
-        self.clipboard_content = new_text
-        self.ui.edtClipboard.setPlainText(self.clipboard_content)
+        self.ui.edtClipboard.setPlainText(new_text)
+
+    def on_actionStart_triggered(self):
+        pass
+
+    def on_actionStop_triggered(self):
+        pass
+
+    def on_txtClipboard_changed(self):
+        new_text = self.ui.edtClipboard.toPlainText()
+        flag = new_text is not None and new_text.endswith(".m3u8")
+        self.can_start.emit(flag)
+
+    def on_can_start(self, flag: bool):
+        self.ui.actionStart.setEnabled(flag)
+
+    def on_can_stop(self, flag: bool):
+        self.ui.actionStop.setEnabled(flag)
 
 
 def run():
